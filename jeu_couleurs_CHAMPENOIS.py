fda6e5541306621e67304e57824d59b8e6b45107
uvsq22004516 <71332537+uvsq22004516@users.noreply.github.com>
2021-04-05 15:38:11 +0200

    Update jeu_couleurs_CHAMPENOIS.py

--- jeu_couleurs_CHAMPENOIS.py
+++ jeu_couleurs_CHAMPENOIS.py
@@ -1,13 +1,41 @@
-################## informations liées au groupe #######################
+#consignes : https://moodle.uvsq.fr/moodle2021/pluginfile.php/198168/mod_resource/content/1/jeu_couleurs.pdf
+################## Informations liées au groupe #######################
 
 # groupe DLMP 5
-# Gabriel CHAMPENOIS
-# Alexis BODEN
-# Lucas LEMARECHAL
-# Djenna SEDDOUGUI
-# Lucien SAVEY
-# Alberic BEAUSSANT
+# BEAUSSANT Alberic 
+# BODEN Alexis 
+# CHAMPENOIS Gabriel 
+# LEMARECHAL Lucas 
+# SAVEY Lucien 
+# SEDDOUGUI Djenna 
 
-# https://github.com/uvsq22004516/projet_incendie
+# https://github.com/uvsq22004516/jeu_couleurs
 
-#########################################
+#######################################################################
+################## Fonctionnement du programme ########################
+
+
+ 
+#######################################################################
+
+################## import des librairies ##############################
+import tkinter as tk
+import random as rd
+
+root = tk.Tk()
+root.title("Jeu de couleurs (IN200 - projet n°2 - DLMP 5)")
+
+################## définition des variables globales ##################
+
+
+################## définition des fonctions ###########################
+
+
+################## définition des widgets #############################
+
+
+################## événements liés aux widgets et 
+#                  appel à la boucle de gestion des événements ########
+
+
+root.mainloop()
