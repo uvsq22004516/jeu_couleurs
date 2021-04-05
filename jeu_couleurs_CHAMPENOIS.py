f8ae3fefcc52c0197b404f7e23985ac2b82f09d1
uvsq22004516 <71332537+uvsq22004516@users.noreply.github.com>
2021-04-05 15:08:14 +0200

    Create jeu_couleurs_CHAMPENOIS.py

---
+++ jeu_couleurs_CHAMPENOIS.py
@@ -0,0 +1,13 @@
+################## informations liées au groupe #######################
+
+# groupe DLMP 5
+# Gabriel CHAMPENOIS
+# Alexis BODEN
+# Lucas LEMARECHAL
+# Djenna SEDDOUGUI
+# Lucien SAVEY
+# Alberic BEAUSSANT
+
+# https://github.com/uvsq22004516/projet_incendie
+
+#########################################
