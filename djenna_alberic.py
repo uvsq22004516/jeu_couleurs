993202f61729547434eb1b2b5116c5d869e267b7
Alberic BEAUSSANT <alberic.beaussantpens.uvsq.fr>
2021-05-08 23:04:20 +0200

    Fct Couleurs/Mots

--- djenna_alberic.py
+++ djenna_alberic.py
@@ -1,7 +1,7 @@
 
 import random as rd
 
-#Définition Variables pour la fonction
+#Définition Variables pour la fonction couleurs
 
 rouge = "firebrick2"
 bleu = "deep sky blue"
@@ -14,15 +14,17 @@ blanc = "snow"
 couleurs_choisies = []
 couleurs = [rouge, bleu, vert, rose, orange, jaune, blanc]
 
+var_couleurs = rd.randint(1, 6)
 
-#
+
 niveau = 2
-#
+
+
+
 def fct_couleurs() :
 
-    var_couleurs = rd.randint(1, 6)
-
-
+    
+   
     if niveau == 1 :
         print(couleurs[var_couleurs])
     
@@ -31,12 +33,36 @@ def fct_couleurs() :
         for i in range(var_couleurs, 0, -1) :
             rd.shuffle(couleurs)
             couleurs_choisies.append(couleurs[i])
-            print(couleurs_choisies)
             couleurs.remove(couleurs[i])
-            print(couleurs)
-            
+
+        print(couleurs_choisies)    
+        print(couleurs)    
         print(var_couleurs)
     
-
-
 fct_couleurs()
+
+
+#Definition Variables pour la fonction mots
+
+mots = ["rouge", "Bleu", "Vert", "Rose", "Orange", "Jaune", "Blanc"]
+mots_choisis = []
+
+
+def fct_mots() :
+
+    if niveau == 1 : 
+
+        print(mots[var_couleurs])
+
+    else : 
+        for i in range(var_couleurs, 0, -1) :
+            rd.shuffle(mots)
+            mots_choisis.append(mots[i])
+            mots.remove(mots[i])
+
+    print(mots_choisis)            
+    print(mots)
+    print(var_couleurs)
+
+
+fct_mots()
