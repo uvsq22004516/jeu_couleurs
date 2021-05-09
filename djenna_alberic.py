81f7fa4d2067653fb052dd1712e1dc29339fc215
Alberic BEAUSSANT <alberic.beaussantpens.uvsq.fr>
2021-05-09 12:32:26 +0200

    Finalisation Fcts

--- djenna_alberic.py
+++ djenna_alberic.py
@@ -10,11 +10,9 @@ rose = "HotPink1"
 orange = "dark orange"
 jaune = "goldenrod2"
 blanc = "snow"
-    
-couleurs_choisies = []
-couleurs = [rouge, bleu, vert, rose, orange, jaune, blanc]
 
-var_couleurs = rd.randint(0, 6)
+
+var_couleurs = rd.randint(1, 7)
 
 
 niveau = 2
@@ -23,21 +21,40 @@ niveau = 2
 
 def fct_couleurs() :
 
-    
+    global var_couleurs
+
+    global couleurs
+    couleurs = [rouge, bleu, vert, rose, orange, jaune, blanc]
+
+    global couleurs_choisies
+    couleurs_choisies = []
+
    
     if niveau == 1 :
-        print(couleurs[var_couleurs])
-    
+        rd.shuffle(couleurs)
+        couleurs_choisies = couleurs[1]
 
     else :
-        for i in range(var_couleurs, 0, -1) :
-            rd.shuffle(couleurs)
-            couleurs_choisies.append(couleurs[i])
-            couleurs.remove(couleurs[i])
 
-        print(couleurs_choisies)    
-        print(couleurs)    
-        print(var_couleurs)
+        if var_couleurs < 7 :
+
+            for i in range(var_couleurs, 0, -1) :
+
+                rd.shuffle(couleurs)
+                couleurs_choisies.append(couleurs[i])
+                couleurs.remove(couleurs[i])
+            print(var_couleurs)
+
+        else :
+
+            rd.shuffle(couleurs)
+            couleurs_choisies = couleurs 
+            print(var_couleurs)
+            print(couleurs)
+
+    print(couleurs_choisies)    
+       
+   
     
 fct_couleurs()
 
@@ -45,24 +62,39 @@ fct_couleurs()
 #Definition Variables pour la fonction mots
 
 mots = ["Rouge", "Bleu", "Vert", "Rose", "Orange", "Jaune", "Blanc"]
-mots_choisis = []
 
 
 def fct_mots() :
 
-    if niveau == 1 : 
+    global mots_choisis
+    mots_choisis = []
 
-        print(mots[var_couleurs])
+    if niveau == 1 :
+        rd.shuffle(mots)
+        mots_choisis = mots[1]
+
 
     else : 
-        for i in range(var_couleurs, 0, -1) :
-            rd.shuffle(mots)
-            mots_choisis.append(mots[i])
-            mots.remove(mots[i])
 
-    print(mots_choisis)            
-    print(mots)
-    print(var_couleurs)
+        if var_couleurs < 7 :
+
+            
+
+            for i in range(var_couleurs, 0, -1) :
+                rd.shuffle(mots)
+                mots_choisis.append(mots[i])
+                mots.remove(mots[i])
+
+        else :
+
+            rd.shuffle(mots)
+            mots_choisis = mots
+            print(var_couleurs)
+            print(mots)
+            
+    print(mots_choisis)
+
+                
 
 
 fct_mots()
