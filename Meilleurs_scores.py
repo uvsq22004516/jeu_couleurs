70e3e56f65540dc0af83ebd86f192cbad495a434
uvsq22010219 <72226795+uvsq22010219@users.noreply.github.com>
2021-05-24 01:45:53 +0200

    Create Meilleurs_scores.py

---
+++ Meilleurs_scores.py
@@ -0,0 +1,18 @@
+nombre_parties = 0
+if nombre_parties <= 10 : 
+    scores = [].append(score_tot)
+    scores.sort(reverse=True)
+    fic = open("Meilleurs scores", "w")
+    fic.write("Dix meilleurs scores :", print(scores))
+else nombre_parties > 10 :
+     scores = [].append(score_tot)
+     scores.sort(reverse=True)
+     for i in range (1, len(scores)+1 ) : 
+         if score_tot <= scores[-1] :
+             pass
+        elif score_tot > scores[-1] :
+            del scores[-1]
+            scores.append(score_tot)
+            scores.sort(reverse=True)
+    fic = open("Meilleurs scores", "w")
+    fic.write("Dix meilleurs scores :", print(scores))
