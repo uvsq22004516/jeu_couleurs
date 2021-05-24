313dd862932ed64648b2103efa1b0d0ccb33d01d
Djenna SEDDOUGUI <djenna.seddougui@ens.uvsq.fr>
2021-05-24 02:02:08 +0200

    change1

--- Meilleurs_scores.py
+++ Meilleurs_scores.py
@@ -3,16 +3,20 @@ if nombre_parties <= 10 :
     scores = [].append(score_tot)
     scores.sort(reverse=True)
     fic = open("Meilleurs scores", "w")
-    fic.write("Dix meilleurs scores :", print(scores))
-else nombre_parties > 10 :
-     scores = [].append(score_tot)
-     scores.sort(reverse=True)
-     for i in range (1, len(scores)+1 ) : 
-         if score_tot <= scores[-1] :
+    fic.write("Dix meilleurs scores :")
+    for i in range len(scores) :
+        fic.write(str(scores[i]))
+elif nombre_parties > 10 :
+    scores = [].append(score_tot)
+    scores.sort(reverse=True)
+    for i in range (1, len(scores)+1 ) : 
+        if score_tot <= scores[-1] :
              pass
         elif score_tot > scores[-1] :
             del scores[-1]
             scores.append(score_tot)
             scores.sort(reverse=True)
     fic = open("Meilleurs scores", "w")
-    fic.write("Dix meilleurs scores :", print(scores))
+    fic.write("Dix meilleurs scores :")
+    for i in range len(scores) :
+        fic.write(str(scores[i]))
